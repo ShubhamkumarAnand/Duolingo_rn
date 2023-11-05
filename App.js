cdb1280c27d5292fdcfce97e31fac4cb9e43a5d3
imskanand <the.anand3099@gmail.com>
2023-11-05 19:28:04 +0530

    React Native basic components

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import icon from './assets/icon.png'
 
 const App = () => {
   const appName = 'Duolingo'
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Welcome to the {appName}!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text} numberOfLines={2}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </Text>
+      <Image source={{
+        uri: "https://avatars.githubusercontent.com/u/56271682?v=4"
+      }} alt='splash-icon' style={{width: 60, height: 100, backgroundColor:'blue'}} resizeMode='repeat'/>
+      <TextInput placeholder='Email'/>
+      <StatusBar style="auto"/>
     </View>
   );
 }
@@ -16,13 +24,14 @@ export default App
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   text: {
-    color: '#fff',
     fontWeight: '900',
-    fontSize: 21
+    fontSize: 21,
+    marginVertical: 10,
+    padding: 5
   }
 });
